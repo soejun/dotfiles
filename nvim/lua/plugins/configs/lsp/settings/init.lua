c610d39d1f50d166e785d3af35815563b085b76f
soejun <chan.wilson.wc@gmail.com>
2024-06-13 12:47:50 -0400

    remove nvim from repo, separate repository

--- nvim/lua/plugins/configs/lsp/settings/init.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-
-M.html = require("plugins.configs.lsp.settings.html")
-M.lua = require("plugins.configs.lsp.settings.lua")
-M.yaml = require("plugins.configs.lsp.settings.yaml")
-
-return M
