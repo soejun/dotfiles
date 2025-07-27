03671cc556ef87d27bd4ea2ea8fcc9c359199781
soejun <chan.wilson.wc@gmail.com>
2025-07-27 10:25:39 -0400

    Change aerospace border colors

--- scripts/aerospace-restart-borders.sh
+++ scripts/aerospace-restart-borders.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Because the thing keeps crashing
-borders active_color=0xff8ec07c inactive_color=0x00000000 width=2.0 style=round hidpi=on order=above
+borders active_color=0xff83a598 inactive_color=0x00000000 width=4.0 style=round hidpi=on order=above
