b780aeb4527276d7dbd2c049add5cb061e198322
soejun <chan.wilson.wc@gmail.com>
2026-08-21 11:10:28 -0400

    fix

--- scripts/clean-nvim.sh
+++ scripts/clean-nvim.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 
 rm -rf "${HOME}/.local/state/nvim"
-# exclude dadbod_ui
-find "${HOME}/.local/share/nvim" -mindepth 1 -maxdepth 1 ! -name "dadbod_ui" -exec rm -rf {} +
+rm -rf "${HOME}/.local/share/nvim"
 rm -rf "${HOME}/.cache/nvim"
